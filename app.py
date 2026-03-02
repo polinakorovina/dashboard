274d5d0103a2ec4fd8557bfff7d3a9fdd7ac1cca
polinakorovina <korovinapolina418@gmail.com>
2026-03-02 20:53:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ st.set_page_config(page_title="Аналитика дежурств", layout="wid
 st.markdown(f"""
     <style>
     /* 1. Цвет фона боковой панели и текста в ней */
-    [data-testid="stSidebar"] {{
-        background-color: #6244BB;
+    [data-testid="stSidebar"] {
+    background-color: #6244BB;
+    color: white;
     }}
     [data-testid="stSidebar"] {{
         color: white;
