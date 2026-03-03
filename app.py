3fdda1fb3ea357264e80ed852e8211f4a16de8a8
polinakorovina <korovinapolina418@gmail.com>
2026-03-03 19:04:10 +1000

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ st.markdown(
         text-overflow: ellipsis;
     }
     .block-container {
-        padding-top: 0.5rem !important;
+        padding-top: 1.0rem !important;
     }
 
     .kpi-value{
