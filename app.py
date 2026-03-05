3e1ffa1ba59338a9ac1483df974b846f08a908dc
polinakorovina <korovinapolina418@gmail.com>
2026-03-05 20:52:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ st.markdown(
 
     /* Компактные отступы страницы */
     .block-container { 
-        padding-top: 0.65rem !important;
+        padding-top: 1.65rem !important;
         padding-bottom: 0.65rem !important;
     }
 
