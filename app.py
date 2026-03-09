a734922a291c3ee65a4520359294c3aabc755c71
polinakorovina <korovinapolina418@gmail.com>
2026-03-09 23:24:53 +1000

    Update app.py

--- app.py
+++ app.py
@@ -444,7 +444,7 @@ with b1:
         color_discrete_sequence=["#6244BB"],
         template="plotly_white"
     )
-    fig_d.update_layout(height=250, xaxis_title=None, margin=dict(l=20, r=20, t=10, b=10))
+    fig_d.update_layout(height=170, xaxis_title=None, margin=dict(l=20, r=20, t=10, b=10))
     st.plotly_chart(fig_d, use_container_width=True)
 
 with b2:
@@ -472,7 +472,7 @@ with b2:
     )
 
     fig_pp.update_layout(
-        height=170,
+        height=250,
         xaxis_title=None,
         yaxis_title=None,
         margin=dict(l=20, r=20, t=20, b=10),
