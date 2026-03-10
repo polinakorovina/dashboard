7fb96c1bb993b2eb2798e4f8b95ee645fec5eb58
polinakorovina <korovinapolina418@gmail.com>
2026-03-10 21:49:31 +1000

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ st.markdown(
 
     /* ===================== LAYOUT ===================== */
     .block-container {
-        padding-top: 2.0rem !important;
+        padding-top: 0.3rem !important;
         padding-bottom: 0.65rem !important;
         padding-left: 1.2rem !important;
         padding-right: 1.2rem !important;
